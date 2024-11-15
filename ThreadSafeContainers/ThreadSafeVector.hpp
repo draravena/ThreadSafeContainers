a8fd9c6284483788aba11f8915c588a7d618fc4d
David Avena <b2026draravena@pshs.edu.ph>
2024-11-16 02:12:58 +0800

    Prototype changes
    -Added overload for []
    -Created helper function for setting using [] operator
    -Made dynamic function thingy depending on whether T is constructable or not
    -Added basic getters
    -Added libraries chrono, functional, type_traits, stdexcept
    -Changed vector_config_t to vc_t
    -Changed enum order (May remove some)

--- ThreadSafeContainers/ThreadSafeVector.hpp
+++ ThreadSafeContainers/ThreadSafeVector.hpp
@@ -15,6 +15,10 @@
 #include <cstdint>
 #include <optional>
 #include <Windows.h>
+#include <chrono>
+#include <functional>
+#include <type_traits>
+#include <stdexcept>
 
 /*
 IMPLEMENT:
@@ -30,77 +34,122 @@ IMPLEMENT:
 */
 
 namespace datatype {
-	typedef enum class vector_config_t : uint64_t {
+	typedef enum class vc_t : uint64_t {
+		// CORE
 		NO_CONFIG = 0x00,
 		DANGEROUS = 0x01,
 		READ_HEAVY = 0x02,
 		WRITE_HEAVY = 0x04,
 		ENABLE_MUTEX_TIMEOUT = 0x08,
-		SNAPSHOT_ENABLED = 0x10,
-		UNLOCKED_READING = 0x20,
-		TRANSACTION_MODE = 0x40,
-		ONLY_GLOBAL_MUTEX = 0x80,
-		DISABLE_READING_UNTIL_FULL = 0x100,
-		DISABLE_WRITING_UNTIL_FULL = 0x200,
-		ENABLE_STATS = 0x400,
-		ENABLE_SPINLOCK = 0x800,
-		RESERVE_MAX_SIZE = 0x1000
-	} vector_config_t ;
+		ONLY_GLOBAL_MUTEX = 0x10,
+		RESERVE_MAX_SIZE = 0x20,
+		// NICHE
+		SNAPSHOT_ENABLED = 0x40,
+		DISABLE_READING_UNTIL_FULL = 0x80,
+		DISABLE_WRITING_UNTIL_EMPTY = 0x100,
+		// IDK
+		UNLOCKED_READING = 0x200, // ?
+		TRANSACTION_MODE = 0x400, // ?
+		ENABLE_SPINLOCK = 0x800, // ?
+		// DEBUG
+		ENABLE_STATS = 0x1000,
+		THROW_EXCEPTION = 0x2000
+		
+		
+		
+	} vc_t ;
 
 	// Bitwise OR operator
-	constexpr bool operator&(vector_config_t lhs, vector_config_t rhs) {
+	constexpr bool operator&(vc_t lhs, vc_t rhs) {
 		return (static_cast<uint64_t>(lhs) & static_cast<uint64_t>(rhs));
 	}
+
 	
 
 	template <typename T = int>
 	class ThreadSafeVector {
 	public:
 		// CONSTRUCTORS
-		ThreadSafeVector(uint64_t maxSize = UINT64_MAX, vector_config_t CONFIG = vector_config_t::NO_CONFIG);
-		ThreadSafeVector(vector_config_t CONFIG = vector_config_t::NO_CONFIG);
+		ThreadSafeVector(uint64_t maxSize = UINT64_MAX, vc_t CONFIG = vc_t::NO_CONFIG);
+		ThreadSafeVector(vc_t CONFIG = vc_t::NO_CONFIG);
 		// VECTOR MODIFIER/ACCESSORS
-		bool push_back(T&& item);
-		bool push_back(T& item);
-		bool pop_back();
-		std::optional<T> access(uint64_t index);
-		bool set(uint64_t index, T& val);
-		bool set(uint64_t index, T&& val);
-		int64_t move(ThreadSafeVector& destination, bool explicitAll = true);
+		bool push_back(T&& item);                     // NO IMPLEMENTATION 
+		bool push_back(T& item);                     // NO IMPLEMENTATION 
+		bool pop_back();                     // NO IMPLEMENTATION 
+		std::optional<T> access(uint64_t index);                     // NO IMPLEMENTATION 
+		bool set(uint64_t index, T& val);                     // NO IMPLEMENTATION 
+		bool set(uint64_t index, T&& val);                     // NO IMPLEMENTATION 
+		int64_t move(ThreadSafeVector& destination, bool explicitAll = true);                     // NO IMPLEMENTATION 
 		// OPERATOR OVERLOADS
+		//const T& operator[](uint64_t index) const;
+		T& operator[](uint64_t index);
 		// VECTOR GETTER FUNCTIONS
 		uint64_t size();
+		bool resizeable();
 		uint64_t maxSize();
 		uint64_t remaining();
 		// CONFIG SETTER FUNCTIONS
 		// CONFIG GETTER FUNCTIONS
-		bool snapshotEnabled();
-		bool spinlockEnabled();
-		bool statsEnabled();
-		bool readingUnlocked();
-		bool dangerous();
-		bool transaction_mode();
+		bool snapshotEnabled();                     // NO IMPLEMENTATION 
+		bool spinlockEnabled();                     // NO IMPLEMENTATION 
+		bool statsEnabled();                     // NO IMPLEMENTATION 
+		bool readingUnlocked();                     // NO IMPLEMENTATION 
+		bool dangerous();                     // NO IMPLEMENTATION 
+		bool transaction_mode();                     // NO IMPLEMENTATION 
 		static uint64_t getTotalRAM();
 		static uint64_t getFreeRAM();
+		
 	private:
 		// CONFIG
-		vector_config_t CONFIG_;
-		static MEMORYSTATUSEX getMemStatusEX();
+		vc_t CONFIG_;
+		bool resizeable_;
+		std::chrono::microseconds timedMutexWaitDuration_;
 		// FUNCTIONS
-		void init(uint64_t maxSize = UINT64_MAX, vector_config_t CONFIG = vector_config_t::NO_CONFIG);
-		void updateSize();
-		void updateMutexMap();
-		std::mutex& mtx();
+		void init(uint64_t maxSize = UINT64_MAX, vc_t CONFIG = vc_t::NO_CONFIG);
+		void updateMutexMap();                     // NO IMPLEMENTATION 
+		static MEMORYSTATUSEX getMemStatusEX();
+		inline std::vector<T>& vec();                     // NO IMPLEMENTATION 
+		T* data();
 		// CONCURRENCY
 		uint64_t elementsPerMutex_ = UINT64_MAX;
-		std::unique_ptr<std::unordered_map<uint64_t, std::shared_ptr<std::mutex>>> mutex_map_;
-		std::mutex globalmtx_;
-		// i dunno
-		std::vector<T>& vec();
-		uint64_t maxSize_;
+		std::unordered_map<uint64_t, std::shared_ptr<std::mutex>> mutex_map_;                     // NO IMPLEMENTATION 
+		mutable std::mutex globalmtx_;
+		mutable std::timed_mutex global_timedmtx_;
+		// VECTOR
+		std::atomic<uint64_t> maxSize_;
 		std::atomic<uint64_t> size_;
 		std::vector<T> vector_;
-
+		// HELPER FUNCTIONS
+		inline bool isConfigured(vc_t CONFIG) const {
+			return (CONFIG & CONFIG_);
+		}
+		inline bool isNOTConfigured(vc_t CONFIG) const {
+			return !(CONFIG & CONFIG_);
+		}
+		inline void throwException(const char* message) {
+			if (isConfigured(vc_t::THROW_EXCEPTION)) {
+				throw std::exception(message);
+			}
+		}
+		// OVERLOAD FUNCTIONS
+		
+		T& OVERLOAD_set(uint64_t index);
+		template <typename U = T>
+		typename std::enable_if<std::is_default_constructible<U>::value, T&>::type
+		OVERLOAD_condition_set(uint64_t index) {
+			if (isConfigured(vc_t::DISABLE_WRITING_UNTIL_EMPTY) && vector_.size() > 0) {
+				T dummy;
+				return dummy;
+			}
+		}
+		template <typename U = T>
+		typename std::enable_if<!std::is_default_constructible<U>::value, T&>::type
+		OVERLOAD_condition_set(uint64_t index) {
+			if (isConfigured(vc_t::DISABLE_WRITING_UNTIL_EMPTY) && vector_.size() > 0) {
+				int dummy = 0;
+				return dummy;
+			}
+		}
 	};
 };
 
--- ThreadSafeContainers/ThreadSafeVector.ipp
+++ ThreadSafeContainers/ThreadSafeVector.ipp
@@ -1,20 +1,60 @@
 #pragma once
-#include "threadSafeVector.hpp"
 
+#include "threadSafeVector.hpp"
+ 
 namespace datatype {
 	template <typename T>
-	ThreadSafeVector<T>::ThreadSafeVector(uint64_t maxSize, vector_config_t CONFIG) {
+	ThreadSafeVector<T>::ThreadSafeVector(uint64_t maxSize, vc_t CONFIG) : resizeable_(false) {
 		init(maxSize, CONFIG);
 	}
 	template <typename T>
-	void ThreadSafeVector<T>::init(uint64_t maxSize, vector_config_t CONFIG) {
-		if (CONFIG & vector_config_t::RESERVE_MAX_SIZE) {
+	ThreadSafeVector<T>::ThreadSafeVector(vc_t CONFIG) : resizeable_(true) {
+
+	}
+	template <typename T>
+	void ThreadSafeVector<T>::init(uint64_t maxSize, vc_t CONFIG) {
+		if (CONFIG & vc_t::RESERVE_MAX_SIZE) {
 			if (maxSize >= getTotalRAM()) {
 				throw std::runtime_error("Allocation size exceeds total memory.");
 			}
 			vector_.reserve(maxSize);
 		}
 	}
+
+	template <typename T>
+	T& ThreadSafeVector<T>::operator[](uint64_t index) {
+		return OVERLOAD_set(index);
+	}
+
+	template <typename T>
+	T& ThreadSafeVector<T>::OVERLOAD_set(uint64_t index) {
+		if (index >= maxSize_) {
+			throwException("Out of bounds. Larger than max size");
+		}
+		if (index >= vector_.size()) {
+			throwException("Out of bounds. Larger than vector size.");
+		}
+		return OVERLOAD_condition_set(index);
+		// IF DANGEROUS, OP WITHOUT MUTEX
+		if (isConfigured(vc_t::DANGEROUS)) {
+			return vector_[index];
+		}
+		// IF GLOBAL MUTEX ONLY AND NO TIMEOUT
+		if (isConfigured(vc_t::ONLY_GLOBAL_MUTEX) && isNOTConfigured(vc_t::ENABLE_MUTEX_TIMEOUT)) {
+			std::lock_guard<std::mutex> lock(globalmtx_);
+			return vector_[index];
+		}
+		if (isConfigured(vc_t::ONLY_GLOBAL_MUTEX) && isConfigured(vc_t::ENABLE_MUTEX_TIMEOUT)) {
+			if (global_timedmtx_.try_lock_for(timedMutexWaitDuration_)) {
+				return vector_[index];;
+			}
+		}
+		if (isConfigured(vc_t::ONLY_GLOBAL_MUTEX) && isConfigured(vc_t::ENABLE_MUTEX_TIMEOUT)) {
+
+		}
+	}
+
+	// GETTERS
 	template <typename T>
 	uint64_t ThreadSafeVector<T>::getTotalRAM() {	
 		MEMORYSTATUSEX msx = getMemStatusEX();
@@ -31,5 +71,25 @@ namespace datatype {
 		memInfo.dwLength = sizeof(MEMORYSTATUSEX);
 		GlobalMemoryStatusEx(&memInfo);
 		return memInfo;
+	}
+	template <typename T>
+	bool ThreadSafeVector<T>::resizeable() {
+		return resizeable_;
+	}
+	template <typename T>
+	uint64_t ThreadSafeVector<T>::size() {
+		return size_;
+	}	
+	template <typename T>
+	uint64_t ThreadSafeVector<T>::maxSize() {
+		return maxSize_;
+	}
+	template <typename T>
+	uint64_t ThreadSafeVector<T>::remaining() {
+		return maxSize_ - size_;
+	}
+	template <typename T>
+	T* ThreadSafeVector<T>::data() {
+		return vector_.data();
 	}
 };
